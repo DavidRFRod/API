244786c16d5f454f76c16110536b62f5127f19ac
David Rodrigues <davidrapha@hotmail.com>
2020-06-27 18:38:38 -0300

    cadastrar e listar

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,31 +4,40 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Student;
-use App\Http\Resources\Student as StudentResource;
 
 class ApiController extends Controller
 {
     public function create(Request $request){
+        $data = $request->all();
         $students = new Student();
         $students->fname = $request->input('fname');
         $students->lname = $request->input('lname');
         $students->email = $request->input('email');
         $students->password = $request->input('password');
 
-        $students->save();
-        return response()->json($students);
+        $students->save($data);
+        return response('OK', 200)
+        ->header('Content-Type', 'text/plain');
 
     }
 
     public function showbyid($id){
 
         $students = Student::find($id);
-        return response()->json($students);
+        return response('OK', 200)
+        ->header('Content-Type', 'text/json');
     }
 
     public function showall(){
 
         $students = Student::all();
-        return StudentResource::collection($students);
+        return response('OK', 200)
+            ->header('Content-Type', 'text/json');
+    }
+    public function delete(Request $request, Student $students){
+
+        $students->delete();
+        return response('OK', 200)
+            ->header('Content-Type', 'text/json');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,42 @@ Route::group(['prefix' => '/v1'], function () {
     Route::get('/students', function(){
         $students = Student::all();
         return $students;
+    });
+    Route::post('/students', function(Request $request){
+        $students = new Student();
+        $students->fill($request->all());
+        if($students->save()){
+            return response('OK', 200)
+            ->header('Content-Type', 'text/json');
+        } else{
+            return response('Erro', 502)
+            ->header('Content-Type', 'text/json');
+        }
+        });
 
-  });
-});
+        Route::delete('/students/{id}', function($id){
+            $s = Student::find($id);
+            if($s->delete()){
+                return response('OK', 200)
+                ->header('Content-Type', 'text/json');
+            }
+       });
+
+       Route::put('/students/{id}', function(Request $request, $id){
+           $students = Student::find($id);
+           $students->fill($request->all());
+           if($students->save()){
+            return response('OK', 200)
+            ->header('Content-Type', 'text/json');
+           }
+       Route::patch('/students/{id}', function(Request $request, $id){
+           $students = Student::find($id);
+           $students->fill($request->all());
+           if($students->save()){
+            return response('OK', 200)
+            ->header('Content-Type', 'text/json');
+           }
+
+            });
+        });
+    });
